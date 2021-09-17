b0a25c113c5369136b643599808b19ac35d098fa
Sam Roble <smroble1996@gmail.com>
2021-09-17 11:44:49 -0600

    project phase 3

--- src/App.js
+++ src/App.js
@@ -1,38 +1,78 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import User from './User';
-import Game from './Game';
+import { useEffect, useState } from "react";
+import "./App.css";
+import User from "./User";
+import Game from "./Game";
 
-const API = 'localhost:3000';
+const API = "http://localhost:9292";
 
 function App() {
   const [users, setUsers] = useState([]);
   const [games, setGames] = useState([]);
 
-  useEffect(() => fetch(`${API}/users`).then((res) => res.json()))
-    .then((json) => setUsers(json))
-    .catch((err) => console.error(err));
+  useEffect(() =>
+    fetch(`${API}/users`)
+      .then((res) => res.json())
+      .then((json) => setUsers(json))
+      .catch((err) => console.error(err), ),[]
+  );
 
-  useEffect(() => fetch(`${API}/games`).then((res) => res.json()))
-    .then((json) => setGames(json))
-    .catch((err) => console.error(err));
+  useEffect(() =>
+    fetch(`${API}/games`)
+      .then((res) => res.json())
+      .then((json) => setGames(json))
+      .catch((err) => console.error(err), ), []
+  );
 
   return (
     <div className="container">
-      <h1>Here's a list of Users:</h1>
+      <h1>Here's a list of the best Developers:</h1>
       <ul>
         {users.map((user) => (
-          <User user={user} />
+          <User key={user.id} user={user} />
         ))}
       </ul>
-      <h1>Here's a list of Games:</h1>
+      <h1>Here's a list of their favorite Games:</h1>
       <ul>
         {games.map((game) => (
-          <Game game={game} />
+          <Game key={game.id} game={game} />
         ))}
       </ul>
     </div>
   );
 }
+
+const app = new function(){
+  this.el=document.getElementById('games');
+  this.games=[]
+
+  this.FetchAll = function(){
+    const data='';
+  };
+
+  this.Add = function(){
+
+  };
+
+  this.Edit =function(item){
+
+  };
+
+  this.Delete = function(item){
+
+  };
+
+  this.Count = function(data){
+
+  };
+
+}
+
+app.FetchAll();
+
+function CloseInput(){
+  document.getElementById('edit-box').style.display = 'none'
+}
+
+
 
 export default App;
